146f51e961bce2beb0c980cfa994ad5838a324e2
darzhz <68496521+darzhz@users.noreply.github.com>
2020-09-14 14:50:11 +0530

    initial file

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const qr = require('qrcode');
+let currentSearch = [];
+const { request, response } = require('express');
+const app = express();
+app.listen(3000, () => {console.log('running at port 3000')});
+app.use(express.static('public'));
+app.use(express.json({limit:'5mb'}))
