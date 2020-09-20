9de96c470f24ac6a08bec675ccefaff115ef5050
darzhz <68496521+darzhz@users.noreply.github.com>
2020-09-20 18:34:43 +0530

    learning git

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const app = express();
 app.listen(3000, () => {console.log('running at port 3000')});
 app.use(express.static('public'));
 app.use(express.json({limit:'5mb'}))
+//hey im learning git
